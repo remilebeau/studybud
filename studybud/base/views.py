60f2217db8a77e8e4073495e407a51d69ad5444f
remilebeau <remilebeau90@gmail.com>
2024-02-17 20:11:21 -0500

    added update user feature

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 # Create your views here.
@@ -185,3 +185,18 @@ def deleteMessage(request, pk):
 
     context = {"obj": message}
     return render(request, "base/delete.html", context)
+
+
+@login_required(login_url="login")
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect("user-profile", pk=user.id)
+
+    context = {"form": form}
+    return render(request, "base/update-user.html", context)
