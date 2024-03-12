2038c0cc85452c5589aaaa8d8b94b40440754768
remilebeau <remilebeau90@gmail.com>
2024-03-12 15:50:58 -0400

    Added https://studybud-0egb.onrender.com to allowed hosts

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-#1qhfjxjo1!e0xyor37*r7fjab0$03-*v6j1!l=n=prl_wbqkt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://studybud-0egb.onrender.com"]
 
 
 # Application definition
