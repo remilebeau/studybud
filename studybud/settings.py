f9aaf25b745509dd32bc7ed57d35526974d0d09b
remilebeau <remilebeau90@gmail.com>
2024-03-12 15:59:08 -0400

    added corsheaders.middleware.CorsMiddleware to middleware and CORS_ALLOWED_ORIGINS with render.com domain

--- studybud/settings.py
+++ studybud/settings.py
@@ -48,7 +48,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = ["studybud-0egb.onrender.com"]
 
 ROOT_URLCONF = "studybud.urls"
 
